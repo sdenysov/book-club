15afa7036f4021467b8cb070fbd3b472c671a330
mykhailo.zheliaskov <mykhailo.zheliaskov@decerto.pl>
2019-11-01 10:32:47 +0100

    feat[icon] add configurable icon sizing parameter

--- frontend/src/app/share/components/icon/icon.component.ts
+++ frontend/src/app/share/components/icon/icon.component.ts
@@ -1,11 +1,13 @@
+import {TIconSize} from '@@share/models/icon-size';
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-icon',
-  template: '<fa-icon [icon]="name"></fa-icon>',
+  template: '<fa-icon [icon]="name" [size]="size"></fa-icon>',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IconComponent {
 
   @Input() name: string;
+  @Input() size: TIconSize = 'sm';
 }
