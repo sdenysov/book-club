f4a758048fcf6d30b6eeb3c95b1144447acd9c11
mykhailo.zheliaskov <mykhailo.zheliaskov@decerto.pl>
2019-11-17 23:14:15 +0100

    feat[profile] extend user model and display it on profile user name

--- frontend/src/app/profile/pages/profile/profile-page.component.ts
+++ frontend/src/app/profile/pages/profile/profile-page.component.ts
@@ -1,7 +1,15 @@
+import {AuthReduxFacade} from '@@auth/store/auth-redux.facade';
+import {IUser} from '@@share/models/user.model';
 import {Component} from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Component({
   templateUrl: 'profile-page.component.html',
   styleUrls: ['profile-page.component.scss']
 })
-export class ProfilePageComponent {}
+export class ProfilePageComponent {
+
+  user$: Observable<IUser> = this.authReduxFacade.loggedInUser$;
+
+  constructor(private authReduxFacade: AuthReduxFacade) {}
+}
